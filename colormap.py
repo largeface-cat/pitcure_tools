0a77e75ab40471aba1f9ab4c0ca5987285ead646
largeface-cat <lsh200408@outlook.com>
2025-04-16 22:02:29 -0400

    colormap

--- colormap.py
+++ colormap.py
@@ -11,11 +11,13 @@ argparser.add_argument('save_path', type=str, help='Path to save the output')
 argparser.add_argument('colormap', type=str, help='rearrange RGB to any of the 6 colormaps')
 
 args = argparser.parse_args()
+reg_seq = {'R':0, 'G':1, 'B':2}
 
 def colormap_image(image_path:str, save_path:str, colormap:str):
     '''
     image_path: str, path to the image
     save_path: str, path to save the encrypted image
+    colormap: str, colormap to use. Use _ to inverse the color value.
     '''
     img = Image.open(image_path)
     img = np.asarray(img)
@@ -26,20 +28,27 @@ def colormap_image(image_path:str, save_path:str, colormap:str):
         img = img[:, :, :3]
     else:
         transparency = None
-    if colormap == 'RGB':
-        pass
-    elif colormap == 'BGR':
-        img = img[:, :, ::-1]
-    elif colormap == 'RBG':
-        img = img[:, :, [0, 2, 1]]
-    elif colormap == 'BRG':
-        img = img[:, :, [2, 0, 1]]
-    elif colormap == 'GBR':
-        img = img[:, :, [1, 2, 0]]
-    elif colormap == 'GRB':
-        img = img[:, :, [1, 0, 2]]
-    else:
-        raise ValueError('Invalid colormap')
+    new_img = np.zeros_like(img)
+    ptr = 0
+    flag = 1
+    for i in colormap:
+        if i == '_':
+            if flag == -1:
+                print('Invalid colormap')
+                return
+            flag = -1
+        else:
+            if i not in reg_seq:
+                print('Invalid colormap')
+                return
+            target = reg_seq[i]
+            if flag == 1:
+                new_img[:, :, ptr] = img[:, :, target]
+            else:
+                new_img[:, :, ptr] = 255 - img[:, :, target]
+            ptr += 1
+            flag = 1
+    img = new_img
     if transparency is not None:
         img = np.concatenate((img, transparency[:, :, np.newaxis]), axis=2)
     img = np.uint8(np.round(img))
